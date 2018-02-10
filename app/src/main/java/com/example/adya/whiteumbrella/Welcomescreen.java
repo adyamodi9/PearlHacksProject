24c659e78999228e1969b0d34f30d8f80d6913fa
kleroux3@gatech.edu <bobert97>
2018-02-10 18:04:00 -0500

    made 2 buttons #nothinbutnet

--- app/src/main/java/com/example/adya/whiteumbrella/Welcomescreen.java
+++ app/src/main/java/com/example/adya/whiteumbrella/Welcomescreen.java
@@ -1,7 +1,10 @@
 package com.example.adya.whiteumbrella;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Welcomescreen extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class Welcomescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomescreen);
+
+        ImageButton login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setContentView(R.layout.activity_login_page);
+                //android.content.Intent myIntent = new android.content.Intent(view.getContext(), LoginPage.class);
+                //startActivityForResult(myIntent, 0);
+            }
+        });
+        ImageButton register = (ImageButton) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setContentView(R.layout.activity_login_page);
+            }
+        });
     }
 }
